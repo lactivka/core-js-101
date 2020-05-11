410413905ab1ab6c82773fd5aa24b89cc191f606
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-05-11 22:15:28 +0300

    merge

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -52,8 +52,8 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.create(proto, Object.create(JSON.parse(json)));
 }
 
 
